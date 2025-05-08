dea4ecfb727d0fad9c1aacd78a4fd3dca26cc4cc
28edmondsm <28edmondsm@bwschools.net>
2025-05-08 09:55:01 -0400

    commit

---
+++ main.py
@@ -0,0 +1,205 @@
+import turtle
+import random
+
+screen = turtle.Screen()
+screen.screensize(500,500)
+screen.bgcolor('black')
+
+
+t=turtle.Turtle()
+#menu
+t.penup()
+t.goto(0,200)
+t.pendown()
+t.pencolor('white')
+t.write("All About me",font=("arial",30,"normal"),align = "center")
+
+
+t.penup()
+t.goto(0, 0)
+t.pendown()
+screen.addshape('mtv downtown.gif')
+t.shape('mtv downtown.gif')
+t.stamp()
+t.shape('classic')
+round = input("Press Enter To Continue: ")
+t.clear()
+screen.bgcolor('red')
+#menu
+
+t.penup()
+t.goto(0,200)
+t.pendown()
+t.pencolor('white')
+t.write("favortie foods",font=("arial",30,"normal"),align = "center")
+
+
+t.penup()
+t.goto(0, 0)
+t.pendown()
+screen.addshape('noodles.gif')
+t.shape('noodles.gif')
+t.stamp()
+t.shape('classic')
+
+t.penup()
+t.goto(100, 100)
+t.pendown()
+screen.addshape('rasta pasta.gif')
+t.shape('rasta pasta.gif')
+t.stamp()
+t.shape('classic')
+
+t.penup()
+t.goto(200, 200)
+t.pendown()
+screen.addshape('strawberries.gif')
+t.shape('strawberries.gif')
+t.stamp()
+t.shape('classic')
+
+
+
+round = input("Press Enter To Continue: ")
+t.clear()
+screen.bgcolor('green')
+t.penup()
+t.goto(0,200)
+t.pendown()
+t.pencolor('white')
+t.write("Hobbies",font=("arial",30,"normal"),align = "center")
+
+
+t.penup()
+t.goto(0, 0)
+t.pendown()
+screen.addshape('bible study.gif')
+t.shape('bible study.gif')
+t.stamp()
+t.shape('classic')
+
+t.penup()
+t.goto(100, 100)
+t.pendown()
+screen.addshape('cooking.gif')
+t.shape('cooking.gif')
+t.stamp()
+t.shape('classic')
+
+t.penup()
+t.goto(200, 200)
+t.pendown()
+screen.addshape('flag.gif')
+t.shape('flag.gif')
+t.stamp()
+t.shape('classic')
+
+t.penup()
+t.goto(300, 300)
+t.pendown()
+screen.addshape('shopping.gif')
+t.shape('shopping.gif')
+t.stamp()
+t.shape('classic')
+
+
+t.penup()
+t.goto(0, 0)
+t.pendown()
+screen.addshape('mtv downtown.gif')
+t.shape('mtv downtown.gif')
+t.stamp()
+t.shape('classic')
+round = input("Press Enter To Continue: ")
+t.clear()
+screen.bgcolor('brown')
+#menu
+
+
+t.penup()
+t.goto(0,200)
+t.pendown()
+t.pencolor('white')
+t.write("favortie movies",font=("arial",30,"normal"),align = "center")
+
+
+t.penup()
+t.goto(0, 0)
+t.pendown()
+screen.addshape('annie.gif')
+t.shape('annie.gif')
+t.stamp()
+t.shape('classic')
+
+t.penup()
+t.goto(100, 100)
+t.pendown()
+screen.addshape('6333.gif')
+t.shape('6333.gif')
+t.stamp()
+t.shape('classic')
+
+t.penup()
+t.goto(200, 200)
+t.pendown()
+screen.addshape('wonder.gif')
+t.shape('wonder.gif')
+t.stamp()
+t.shape('classic')
+
+t.penup()
+t.goto(300, 300)
+t.pendown()
+screen.addshape('t.h.u.g.gif')
+t.shape('t.h.u.g.gif')
+t.stamp()
+t.shape('classic')
+
+
+
+t.penup()
+t.goto(0, 0)
+t.pendown()
+screen.addshape('mtv downtown.gif')
+t.shape('mtv downtown.gif')
+t.stamp()
+t.shape('classic')
+round = input("Press Enter To Continue: ")
+t.clear()
+screen.bgcolor('brown')
+#menu
+
+
+t.penup()
+t.goto(0,200)
+t.pendown()
+t.pencolor('white')
+t.write("favortie sport",font=("arial",30,"normal"),align = "center")
+
+
+t.penup()
+t.goto(0, 0)
+t.pendown()
+screen.addshape('football.1.gif')
+t.shape('football.1.gif')
+t.stamp()
+t.shape('classic')
+
+t.penup()
+t.goto(100, 100)
+t.pendown()
+screen.addshape('football.2.gif')
+t.shape('football.2.gif')
+t.stamp()
+t.shape('classic')
+
+t.penup()
+t.goto(200, 200)
+t.pendown()
+screen.addshape('football.3.gif')
+t.shape('football.3.gif')
+t.stamp()
+t.shape('classic')
+
+
+turtle.done()
